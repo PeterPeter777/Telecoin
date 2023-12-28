060c8de5918003236b8c715ae21b18792e01965c
PeterPeter777 <101781644+PeterPeter777@users.noreply.github.com>
2023-12-28 09:16:43 +0000

    API code

--- Blockchain/dev/api.js
+++ Blockchain/dev/api.js
@@ -1,18 +1,26 @@
 const express = require('node_modules/../express');
 const app = express();
+const bodyParser = require('node_modules/../body-parser');
+const Blockchain = require('./blokchain');
+
+const telecoin = new Blockchain();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.get('/blockchain', function (req, res) {
-  res.send(telecoin);
+  //res.send(telecoin);
 });
 
 app.post('/transaction',function(res, req) {
+  console.log(req.body);
+  res.send('The amount of transaction is' + req.body.amount + ' ' + 'telecoin');
   
 });
 
-app.get('/mine', function (req, res) {
+app.get('/mine', function(req, res) {
   
-
-
 });
 
 app.listen(3000, function() {
