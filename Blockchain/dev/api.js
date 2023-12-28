e4113cbf68797d2cef024723158a7089ef7bfde3
PeterPeter777 <101781644+PeterPeter777@users.noreply.github.com>
2023-12-28 08:37:54 +0000

    Changes in APi folder

--- Blockchain/dev/api.js
+++ Blockchain/dev/api.js
@@ -5,6 +5,14 @@ app.get('/blockchain', function (req, res) {
   res.send(telecoin);
 });
 
+app.post('/transaction',function(res, req) {
+  
+});
 
+app.get('/mine', function (req, res) {
+  
+});
 
-app.listen(3000);
+app.listen(3000, function() {
+  console.log('Listening on port 3000...');
+});
