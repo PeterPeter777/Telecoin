66f92f5eec86bd425f611bdd04eade5f085416ab
PeterPeter777 <101781644+PeterPeter777@users.noreply.github.com>
2023-12-30 20:16:29 +0000

    Installing request library and building Register Api operations

--- Blockchain/dev/networkNode.js
+++ Blockchain/dev/networkNode.js
@@ -5,6 +5,8 @@ const Blockchain = require('./blokchain');
 const uuid = require('node_modules/../uuid/../uuid');
 // acces the 3 posistion in package json -> start script 
 const port = process.argv[2];
+// acces library request-promise
+const rp = require('node_modules/../request');
 
 const nodeAddress = crypto.randomUUID();//().split('-').join('');
 
@@ -44,24 +46,59 @@ app.get('/mine', function(req, res) {
   });
 });
 
+
 // register a node and broadcast it the network
 app.post('/register-and-broadcast-node', function(req,res) {
   const newNodeUrl = req.body.newNodeurl;
-  if(telecoin.networkNodes.indexOf(newNodeurl) == -1) telecoin.networkNodes.push(newNodeUrl);
+  if(telecoin.networkNodes.indexOf(newNodeurl) == -1) telecoin.networkNodes.push(newNodeUrl);//registering
 
+  // request to all registered nodes - uris
+  const regNodesPromises = [];
   telecoin.networkNodes.forEach(networkNodeUrl => {
-    // register-node
+     const requestOptions = {
+         uri: networkNodeUrl + '/register-node',
+         method: 'POST',
+         body: {newNodeUrl: newNodeUrl},
+         json: true
+     };
+     regNodesPromises.push(rp(requestOptions));
+  });
+
+  Promise.all(regNodesPromises)
+  .then(data => {
+    const bulkRegisterOptions = {
+      uri: newNodeUrl + '/register-nodes-bulk',
+      method: 'POST',
+      body: { allNetworkNodes: [ ...telecoin.networkNodes, telecoin.currentNodeUrl ]},
+      json: true
+    };
+
+    return rp(bulkRegisterOptions);
+  })
+  .then(data => {
+      res.json({note: 'New node registered with network successfully.'});
   });
 });
 
 // register a node with the network
 app.post('/register-node', function(req,res) {
-  
+    const newNodeUrl = req.body.newNodeUrl;
+    // error Handeling
+    const nodeNotAlreadyPresent = telecoin.networkNodes.indexOf(newNodeUrl) == -1;
+    const notCurrentNode = telecoin.currentNodeUrl !== newNodeUrl;
+    if (nodeNotAlreadyPresent && notCurrentNode) telecoin.networkNodes.push(newNodeUrl);
+    res.json({ note: 'New node registered successfully with node'});
 });
 
 // register multiple nodes at once
 app.post('/register-nodes-bulk', function(req, res) {
-
+    const allNetworkNodes = req.body.allNetworkNodes;
+    allNetworkNodes.forEach(networkNodeUrl => {
+     const nodeNotAlreadyPresent = telecoin.networkNodes.indexOf(networkNodeUrl) == -1;
+     const notCurrentNode = telcoin.currentNodeUrl !== networkNodeUrl
+      if (nodeNotAlreadyPresent && notCurrentNode) telecoin.networkNodes.push(networkNodeUrl);
+    });
+    res.json({ note: 'Bulk registration successful'});
 });
 
 app.listen(port, function() {
